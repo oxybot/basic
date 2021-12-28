0a9533ae5e81fdf48b1533aba2c9c17216f9b084
superfaz <16510828+superfaz@users.noreply.github.com>
2021-12-28 22:21:17 +0100

    Add image form control

--- basic.front/src/Generic/EntityForm.js
+++ basic.front/src/Generic/EntityForm.js
@@ -29,6 +29,49 @@ function EntityInputClient({ field, value, onChange }) {
   );
 }
 
+function EntityInputImage({ field, value, onChange }) {
+  function handleRemove() {
+    onChange({ target: { name: field.name, value: null } });
+  }
+
+  function handleFileChange(e) {
+    if (e.target.files.length === 0) {
+      // Do nothing: User clicked 'cancel'
+      return;
+    }
+
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const data = e.target.result.replace(/^data:.+;base64,/, "");
+      onChange({ target: { name: field.name, value: { mimeType: file.type, data: data } } });
+    };
+    reader.readAsDataURL(file);
+  }
+
+  return (
+    <div class="d-flex">
+      {value && <img className="avatar avatar-lg me-2" src={`data:${value.mimeType};base64,${value.data}`} />}
+      {!value && <div className="avatar avatar-lg me-2"></div>}
+      <div class="d-flex flex-column align-items-start justify-content-between">
+        <input
+          type="file"
+          className="form-control"
+          name={field.name}
+          required={field.required}
+          accept="image/*"
+          onChange={handleFileChange}
+        />
+        {value && (
+          <button type="button" className="btn btn-secondary mt-1" onClick={handleRemove}>
+            Remove
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
+
 function EntityInput({ field, value, onChange }) {
   switch (field.type) {
     case "date":
@@ -47,6 +90,9 @@ function EntityInput({ field, value, onChange }) {
 
     case "ref/client":
       return <EntityInputClient field={field} value={value} onChange={onChange} />;
+
+    case "image":
+      return <EntityInputImage field={field} value={value} onChange={onChange} />;
 
     case "string":
       return (
