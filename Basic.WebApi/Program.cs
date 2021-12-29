692d13c2958fcb09eb918b8df464b2787e2d0f04
superfaz <16510828+superfaz@users.noreply.github.com>
2021-12-29 10:59:18 +0100

    Make swagger the default page on API

--- Basic.WebApi/Program.cs
+++ Basic.WebApi/Program.cs
@@ -42,8 +42,14 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(options =>
+    {
+    });
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        options.RoutePrefix = "";
+    });
 }
 
 app.UseHttpsRedirection();
