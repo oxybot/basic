80c9011e5fe398dd7755a85e08fe8ddc55e77749
superfaz <16510828+superfaz@users.noreply.github.com>
2021-12-29 10:33:15 +0100

    Add missing documentation

--- Basic.Model/TypedFile.cs
+++ Basic.Model/TypedFile.cs
@@ -3,12 +3,21 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Basic.Model
 {
+    /// <summary>
+    /// Represents a file added to an entity.
+    /// </summary>
     [Owned]
     public class TypedFile
     {
+        /// <summary>
+        /// Gets or sets the data of the file.
+        /// </summary>
         [Required]
         public byte[] Data { get; set; }
 
+        /// <summary>
+        /// Gets or sets the mime-type of the file.
+        /// </summary>
         [Required]
         public string MimeType { get; set; }
     }
--- Basic.WebApi/DTOs/Base64File.cs
+++ Basic.WebApi/DTOs/Base64File.cs
@@ -1,9 +1,18 @@
 ﻿namespace Basic.WebApi.DTOs
 {
+    /// <summary>
+    /// Represents a file shared with a base-64 encoding.
+    /// </summary>
     public class Base64File
     {
+        /// <summary>
+        /// Gets or sets the data of the file as a base-64 string.
+        /// </summary>
         public string Data { get; set; }
 
+        /// <summary>
+        /// Gets or sets the mime-type of the file.
+        /// </summary>
         public string MimeType { get; set; }
     }
 }
