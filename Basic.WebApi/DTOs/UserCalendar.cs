a417ecc2dc1294102dfeabcbb7c6e9c76d893bd8
superfaz <16510828+superfaz@users.noreply.github.com>
2021-12-30 23:19:37 +0100

    Adding documentation

--- Basic.WebApi/DTOs/UserCalendar.cs
+++ Basic.WebApi/DTOs/UserCalendar.cs
@@ -1,25 +1,49 @@
 ﻿namespace Basic.WebApi.DTOs
 {
+    /// <summary>
+    /// Represents the calendar of a specific user.
+    /// </summary>
     public class UserCalendar
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserCalendar"/> class.
+        /// </summary>
         public UserCalendar()
         {
             this.Lines = new List<Line>();
         }
 
+        /// <summary>
+        /// Gets or sets the associated users.
+        /// </summary>
         public EntityReference User { get; set; }
 
+        /// <summary>
+        /// Gets or sets the lines for this user.
+        /// </summary>
         public ICollection<Line> Lines { get; set; }
 
+        /// <summary>
+        /// Represents a calendar line of a specific user and category.
+        /// </summary>
         public class Line
         {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Line"/> class.
+            /// </summary>
             public Line()
             {
                 this.Days = new List<int>();
             }
 
+            /// <summary>
+            /// Gets or sets the category name for this line.
+            /// </summary>
             public string Category { get; set; }
 
+            /// <summary>
+            /// Gets or sets the days part of this calendar line.
+            /// </summary>
             public ICollection<int> Days { get; set; }
         }
     }
