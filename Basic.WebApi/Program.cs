cbfd4b65c0239f69fd4731e35c25caf2dc304d76
superfaz <16510828+superfaz@users.noreply.github.com>
2022-01-08 06:08:53 +0100

    Fix program

--- Basic.WebApi/Program.cs
+++ Basic.WebApi/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.Filters;
 using System.Reflection;
 using System.Text;
 using System.Text.Json.Serialization;
