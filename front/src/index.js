01fa2b500bbabbeb0fc278b5ae6d6293c81db458
superfaz <16510828+superfaz@users.noreply.github.com>
2022-04-02 21:33:38 +0200

    Libraries upgrade

--- front/src/index.js
+++ front/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
@@ -9,15 +9,17 @@ import "@tabler/core/dist/css/tabler.min.css";
 import "./index.css";
 import "@tabler/core/dist/js/tabler.min.js";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
