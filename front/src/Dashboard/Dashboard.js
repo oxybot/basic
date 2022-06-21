b57185e3807e87a4cba9ee9ef9ba59b2c02656bd
KevinG57 <gerber.kevin@live.fr>
2022-06-21 09:23:31 +0200

    [Kevin] essai

--- front/src/Dashboard/Dashboard.js
+++ front/src/Dashboard/Dashboard.js
@@ -106,7 +106,7 @@ export function Dashboard() {
         <div className="page-header d-print-none d-none d-lg-block">
           <div className="row align-items-center">
             <div className="col">
-              <div className="page-pretitle">Overview</div>
+              <div className="page-pretitle">Overview first try</div>
               <h2 className="page-title">Dashboard</h2>
             </div>
           </div>
