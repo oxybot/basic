f344bfceda9dcd66d5f1e6f1c25b566fe91c1fb9
superfaz <16510828+superfaz@users.noreply.github.com>
2022-08-08 16:56:11 +0200

    Add migration for sql server - user email

---
+++ src/Basic.DataAccess.SqlServer/Migrations/20220808145420_AddUserEmail.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Basic.DataAccess.SqlServer.Migrations
+{
+    public partial class AddUserEmail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "User");
+        }
+    }
+}
