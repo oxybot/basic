61af86c03ff4c0969e13c8229179a6c167011672
KevinKGE <gerber.kevin@live.fr>
2022-08-17 18:13:04 +0200

    Fixed display list for window's size

--- front/src/Generic/EntityList.js
+++ front/src/Generic/EntityList.js
@@ -8,7 +8,6 @@ import { refresh as refreshEvents } from "../Events/slice";
 import { refresh as refreshBalances } from "../Balances/slice";
 import { refresh as refreshSchedules } from "../Schedules/slice";
 import { useDispatch } from "react-redux";
-
 function filtered(fields, filter) {
   if (!fields) {
     return fields;
@@ -17,25 +16,27 @@ function filtered(fields, filter) {
 }
 
 export default function EntityList({ loading, definition, entities, baseTo = null, selectedId, filter }) {
+
   const [numberOfRowToDisplay, setNumberOfRowToDisplay] = useState(Math.trunc(window.innerHeight / 40));
   const [pageNumber, setPageNumber] = useState(numberOfRowToDisplay);
 
   window.onscroll = () => {
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
       setPageNumber(pageNumber + 20);
     }
   };
 
   const handleResize = () => {
-    setNumberOfRowToDisplay(window.innerHeight / 40);
-  };
+    setNumberOfRowToDisplay(window.innerHeight / 40)
+  }
 
   useEffect(() => {
-    if (pageNumber < window.innerHeight / 40) {
-      setPageNumber(window.innerHeight / 40);
+    if(pageNumber < (window.innerHeight / 40)) {
+       setPageNumber(window.innerHeight / 40);
     }
-    window.addEventListener("resize", handleResize);
-  }, [pageNumber]);
+    window.addEventListener('resize', handleResize);
+
+  }, [window.innerHeight])
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -46,69 +47,75 @@ export default function EntityList({ loading, definition, entities, baseTo = nul
   useEffect(() => {
     dispatch(refreshUsers(sortValue, sortKey, null));
     dispatch(refreshEvents(sortValue, sortKey, null));
+
+    
+          
+            
+    
+
+          
+    
+    
+  
     dispatch(refreshBalances(sortValue, sortKey, null));
     dispatch(refreshSchedules(sortValue, sortKey, null));
-  }, [sortKey, sortValue, dispatch]);
-
+  }, [sortKey, sortValue, dispatch])
   return (
     <>
-      <div className="table-responsive">
-        <table className="table card-table table-vcenter text-nowrap datatable table-hover">
-          <thead>
-            <tr>
-              {fields &&
+    <div className="table-responsive">
+      <table className="table card-table table-vcenter text-nowrap datatable table-hover">
+        <thead>
+          <tr>
+            {fields &&
+              fields.map((field, index) => (
+                <th 
+                  key={index} 
+                  className={"sorting"} 
+                  onClick={() => {
+                    setSortValue(sortValue==="asc"?"desc":"asc");
+                    setSortKey(field.displayName);
+                  }}
+                >
+                  {field.displayName}    {(sortKey === field.displayName) && sortValue !== null &&
+                                          sortValue==="asc"?<IconArrowNarrowUp/>:
+                                          (sortKey === field.displayName && sortValue==="desc"?<IconArrowNarrowDown/>:null)}
+                </th>
+              ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr className={loading ? "" : "d-none"}>
+            <td colSpan={fields?.length}>
+              <IconLoader /> Loading...
+            </td>
+          </tr>
+          {entities.slice(0, pageNumber).map((entity) => (
+            <tr
+            key={entity.identifier}
+            className={clsx({
+              "table-active": entity.identifier === selectedId,
+            })}
+            onClick={() => baseTo !== null && navigate([baseTo, entity.identifier].filter((i) => i).join("/"))}
+            >
+              {entity &&
+                fields &&
                 fields.map((field, index) => (
-                  <th
-                    key={index}
-                    className={"sorting"}
-                    onClick={() => {
-                      setSortValue(sortValue === "asc" ? "desc" : "asc");
-                      setSortKey(field.displayName);
-                    }}
-                  >
-                    {field.displayName}{" "}
-                    {sortKey === field.displayName && sortValue !== null && sortValue === "asc" ? (
-                      <IconArrowNarrowUp />
-                    ) : sortKey === field.displayName && sortValue === "desc" ? (
-                      <IconArrowNarrowDown />
-                    ) : null}
-                  </th>
+                  <td key={index}>
+                    <EntityFieldView type={field.type} value={entity[field.name]} list />
+                  </td>
                 ))}
             </tr>
-          </thead>
-          <tbody>
-            <tr className={loading ? "" : "d-none"}>
+          ))}
+          {!loading && entities.length === 0 && (
+            <tr>
               <td colSpan={fields?.length}>
-                <IconLoader /> Loading...
+                <em>No results</em>
               </td>
             </tr>
-            {entities.slice(0, pageNumber).map((entity) => (
-              <tr
-                key={entity.identifier}
-                className={clsx({
-                  "table-active": entity.identifier === selectedId,
-                })}
-                onClick={() => baseTo !== null && navigate([baseTo, entity.identifier].filter((i) => i).join("/"))}
-              >
-                {entity &&
-                  fields &&
-                  fields.map((field, index) => (
-                    <td key={index}>
-                      <EntityFieldView type={field.type} value={entity[field.name]} list />
-                    </td>
-                  ))}
-              </tr>
-            ))}
-            {!loading && entities.length === 0 && (
-              <tr>
-                <td colSpan={fields?.length}>
-                  <em>No results</em>
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-      </div>
+          )}
+        </tbody>
+      </table>
+    </div>
     </>
   );
 }
