ab0e826b51bd41208f0fe894e0114f6aea3d0a76
superfaz <16510828+superfaz@users.noreply.github.com>
2022-08-22 16:21:03 +0200

    Code review

--- front/src/api/index.js
+++ front/src/api/index.js
@@ -92,25 +92,26 @@ export async function apiFetch(url, options) {
 export function useApiFetch(url, options, defaultState = null, transform = defaultTransform) {
   const [loading, setLoading] = useState(true);
   const [response, setResponse] = useState(defaultState);
-  const [error, setError] = useState(false);
+  const [errors, setErrors] = useState([]);
   const dispatch = useDispatch();
 
   useEffect(() => {
     apiFetch(url, options)
       .then((response) => {
         setResponse(transform(response));
+        setErrors([]);
         setLoading(false);
       })
       .catch((err) => {
         if (err !== null && err.message === "401") {
           dispatch(disconnect());
         } else {
-          setError(err);
+          setErrors(err);
         }
       });
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [url.toString(), JSON.stringify(options), transform]);
 
-  return [loading, response, error];
+  return [loading, response, errors];
 }
