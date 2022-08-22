9f36e6651e889b937bcbdb5bb4298c1a281f0d07
superfaz <16510828+superfaz@users.noreply.github.com>
2022-08-22 09:55:51 +0200

    Add token migration for sql server

---
+++ src/Basic.DataAccess.SqlServer/Migrations/20220822075210_AddUserToken.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Basic.DataAccess.SqlServer.Migrations
+{
+    public partial class AddUserToken : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Token",
+                columns: table => new
+                {
+                    Identifier = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Expiration = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserIdentifier = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Token", x => x.Identifier);
+                    table.ForeignKey(
+                        name: "FK_Token_User_UserIdentifier",
+                        column: x => x.UserIdentifier,
+                        principalTable: "User",
+                        principalColumn: "Identifier",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Token_UserIdentifier",
+                table: "Token",
+                column: "UserIdentifier");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Token");
+        }
+    }
+}
