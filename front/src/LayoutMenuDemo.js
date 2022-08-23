7774680e4ae138d74199ee8606326cceb5c073fb
superfaz <16510828+superfaz@users.noreply.github.com>
2022-08-23 09:31:20 +0200

    Add a beta switch for demo

--- front/src/LayoutMenuDemo.js
+++ front/src/LayoutMenuDemo.js
@@ -6,24 +6,41 @@ import { setRoles } from "./Authentication/slice";
 export default function LayoutMenuDemo() {
   const dispatch = useDispatch();
   const [select, setSelect] = useState("all");
+  const [beta, setBeta] = useState(true);
 
   function handleChange(event) {
     setSelect(event.target.value);
-    switch (event.target.value) {
+
+    update(event.target.value, beta);
+  }
+
+  function handleBeta(event) {
+    setBeta(event.target.checked);
+    update(select, event.target.checked);
+  }
+
+  function update(select, beta) {
+    const betaArray = beta ? [{ code: "beta" }] : [];
+    switch (select) {
       case "all":
-        apiFetch("My/Roles", { method: "GET" }).then((response) => dispatch(setRoles(response)));
+        apiFetch("My/Roles", { method: "GET" }).then((response) => {
+          if (!beta) {
+            response = response.filter((v) => v.code !== "beta");
+          }
+          dispatch(setRoles(response));
+        });
         break;
 
       case "user":
-        dispatch(setRoles([]));
+        dispatch(setRoles(betaArray.concat([])));
         break;
 
       case "hr":
-        dispatch(setRoles([{ code: "time" }]));
+        dispatch(setRoles(betaArray.concat([{ code: "time" }])));
         break;
 
       case "finance":
-        dispatch(setRoles([{ code: "client" }]));
+        dispatch(setRoles(betaArray.concat([{ code: "client" }])));
         break;
 
       default:
@@ -32,13 +49,28 @@ export default function LayoutMenuDemo() {
   }
 
   return (
-    <li className="nav-item">
-      <select className="form-select" value={select} onChange={handleChange}>
-        <option value="all">all</option>
-        <option value="user">as user</option>
-        <option value="hr">as HR</option>
-        <option value="finance">as Finance</option>
-      </select>
-    </li>
+    <>
+      <li className="nav-item">
+        <select className="form-select" value={select} onChange={handleChange}>
+          <option value="all">all</option>
+          <option value="user">as user</option>
+          <option value="hr">as HR</option>
+          <option value="finance">as Finance</option>
+        </select>
+      </li>
+      <div className="form-check form-switch mt-3">
+        <input
+          className="form-check-input"
+          type="checkbox"
+          role="switch"
+          id="beta"
+          checked={beta}
+          onChange={handleBeta}
+        />
+        <label className="form-check-label" htmlFor="beta">
+          Beta features
+        </label>
+      </div>
+    </>
   );
 }
