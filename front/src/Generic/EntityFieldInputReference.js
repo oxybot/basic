ddb8cc88c69933dc51565bcf216485fd10a29378
superfaz <16510828+superfaz@users.noreply.github.com>
2022-10-08 14:52:43 +0200

    Improve select display

--- front/src/Generic/EntityFieldInputReference.js
+++ front/src/Generic/EntityFieldInputReference.js
@@ -7,20 +7,24 @@ const transform = (elements) =>
     value: c.identifier,
     label: c.displayName,
   }));
+
 export default function EntityFieldInputReference({ baseApiUrl, field, value, hasErrors, onChange }) {
   const get = { method: "GET" };
   const [loading, elements] = useApiFetch(baseApiUrl, get, [], transform);
+
   return (
     !loading && (
       <Select
         name={field.name}
         required={field.required}
+        isClearable={true}
+        isSearchable={true}
         classNamePrefix="react-select"
         className={clsx({ "is-invalid": hasErrors })}
         placeholder={field.placeholder}
         options={elements}
-        value={elements.filter((s) => s.value === value)}
-        onChange={(s) => onChange({ target: { name: field.name, value: s.value } })}
+        defaultValue={elements.filter((s) => s.value === value)}
+        onChange={(s) => onChange({ target: { name: field.name, value: s && s.value } })}
       />
     )
   );
