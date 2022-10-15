9faf2a83f9f01096d60b1d634c3f8da71192b664
superfaz <16510828+superfaz@users.noreply.github.com>
2022-10-15 06:55:24 +0200

    Review EventCategory form

--- src/Basic.WebApi/DTOs/EventCategoryForEdit.cs
+++ src/Basic.WebApi/DTOs/EventCategoryForEdit.cs
@@ -16,20 +16,20 @@ namespace Basic.WebApi.DTOs
         public string DisplayName { get; set; }
 
         /// <summary>
-        /// Gets or sets a value indicating whether a balance is required to book time on this category.
-        /// </summary>
-        [Required]
-        [Display(Description = "With a balance, this type of events will be capped for each user to the maximum of their balance.")]
-        [SwaggerSchema("With a balance, this type of events will be capped for each user to the maximum of their balance.")]
-        public bool? RequireBalance { get; set; }
-
-        /// <summary>
         /// Gets or sets how the time booked on this category should be considered.
         /// </summary>
         [Required]
         [Display(Description = "Choose 'Time-off' for an event that is linked to the absence of the user.")]
         [SwaggerSchema(Format = "ref/eventtimemapping", Description = "Choose 'Time-off' for an event that is linked to the absence of the user.")]
         public EventTimeMapping? Mapping { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether a balance is required to book time on this category.
+        /// </summary>
+        [Required]
+        [Display(Description = "With a balance, this type of events will be capped for each user to the maximum of their balance.")]
+        [SwaggerSchema("With a balance, this type of events will be capped for each user to the maximum of their balance.")]
+        public bool? RequireBalance { get; set; }
 
         /// <summary>
         /// Gets or sets the css class associated to the category.
--- src/Basic.WebApi/DTOs/EventCategoryForList.cs
+++ src/Basic.WebApi/DTOs/EventCategoryForList.cs
@@ -23,17 +23,17 @@ namespace Basic.WebApi.DTOs
         public string DisplayName { get; set; }
 
         /// <summary>
-        /// Gets or sets a value indicating whether a balance is required to book time on this category.
-        /// </summary>
-        [Required]
-        public bool RequireBalance { get; set; }
-
-        /// <summary>
         /// Gets or sets how the time booked on this category should be considered.
         /// </summary>
         [Required]
         [SwaggerSchema(Format = "ref/eventtimemapping")]
         public EventTimeMapping Mapping { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether a balance is required to book time on this category.
+        /// </summary>
+        [Required]
+        public bool RequireBalance { get; set; }
 
         /// <summary>
         /// Gets or sets the css class associated to the category.
