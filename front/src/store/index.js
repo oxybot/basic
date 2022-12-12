ffb03a3f1e139ecaeed92b12b702d965905be2a5
superfaz <16510828+superfaz@users.noreply.github.com>
2022-12-12 20:04:43 +0100

    Remove clients slice

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -3,7 +3,6 @@ import agreementsReducer from "../Agreements/slice";
 import alertsReducer from "../Alerts/slice";
 import authenticationReducer from "../Authentication/slice";
 import balancesReducer from "../Balances/slice";
-import clientsReducer from "../Clients/slice";
 import eventCategoriesReducer from "../EventCategories/slice";
 import eventsReducer from "../Events/slice";
 import globalDaysOffReducer from "../GlobalDaysOff/slice";
@@ -30,7 +29,6 @@ export const store = configureStore(
       agreements: agreementsReducer,
       alerts: alertsReducer,
       balances: balancesReducer,
-      clients: clientsReducer,
       eventCategories: eventCategoriesReducer,
       events: eventsReducer,
       globalDaysOff: globalDaysOffReducer,
