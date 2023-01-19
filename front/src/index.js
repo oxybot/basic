194d3056771557501bf759c38dab67aaa7bda840
superfaz <16510828+superfaz@users.noreply.github.com>
2023-01-19 21:39:40 +0100

    Update yarn dependencies

--- front/src/index.js
+++ front/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { store } from "./store";
 import "@tabler/core/dist/css/tabler.min.css";
 import "./index.scss";
@@ -18,8 +17,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/Basic.WebApi/README.md
+++ src/Basic.WebApi/README.md
@@ -5,6 +5,6 @@ As such all actions available in the web app can be executed via the following A
 
 ## Where to start
 
-- Start by using the /auth to authenticate yourself.
+- Start by using the `/auth` to authenticate yourself.
   The received payload will contain an `accessToken`.
 - Send a new request to /clients with a `Authorization` header set to `Bearer ` followed by the accessToken.
