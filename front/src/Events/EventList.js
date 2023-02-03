349ac43799b349de39e1d46feee5cb75548a7500
superfaz <16510828+superfaz@users.noreply.github.com>
2023-02-03 20:49:09 +0100

    First visual on the filters

--- front/src/Events/EventList.js
+++ front/src/Events/EventList.js
@@ -1,8 +1,72 @@
+import { IconSearch } from "@tabler/icons";
+import { useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
 import { useDefinition } from "../api";
 import { useSorting } from "../helpers/sorting";
+import { useFiltering } from "../helpers/filtering";
 import PageList from "../Generic/PageList";
 import "./card.scss";
+
+function EventFilters({ search, onSearchChange, statuses, onStatusChange }) {
+  return (
+    <>
+      <label className="form-label">Search</label>
+      <div className="input-icon mb-3">
+        <input type="text" className="form-control" placeholder="Search…" value={search} onChange={onSearchChange} />
+        <span className="input-icon-addon">
+          <IconSearch />
+        </span>
+      </div>
+      <label className="form-label">Status</label>
+      <div className="form-selectgroup">
+        <label className="form-selectgroup-item">
+          <input
+            type="checkbox"
+            name="status"
+            value="requested"
+            className="form-selectgroup-input"
+            checked={statuses.requested || false}
+            onChange={onStatusChange}
+          />
+          <span className="form-selectgroup-label">Requested</span>
+        </label>
+        <label className="form-selectgroup-item">
+          <input
+            type="checkbox"
+            name="status"
+            value="approved"
+            className="form-selectgroup-input"
+            checked={statuses.approved || false}
+            onChange={onStatusChange}
+          />
+          <span className="form-selectgroup-label">Approved</span>
+        </label>
+        <label className="form-selectgroup-item">
+          <input
+            type="checkbox"
+            name="status"
+            value="rejected"
+            className="form-selectgroup-input"
+            checked={statuses.rejected || false}
+            onChange={onStatusChange}
+          />
+          <span className="form-selectgroup-label">Rejected</span>
+        </label>
+        <label className="form-selectgroup-item">
+          <input
+            type="checkbox"
+            name="status"
+            value="canceled"
+            className="form-selectgroup-input"
+            checked={statuses.canceled || false}
+            onChange={onStatusChange}
+          />
+          <span className="form-selectgroup-label">Canceled</span>
+        </label>
+      </div>
+    </>
+  );
+}
 
 export function EventList() {
   const { eventId } = useParams();
@@ -14,6 +78,18 @@ export function EventList() {
 
   const elements = useLoaderData();
   const [sorting, updateSorting] = useSorting();
+  const [filters, setFilters] = useFiltering();
+
+  const [search, setSearch] = useState("");
+
+  function handleStatusChange(event) {
+    let target = event.target;
+    setFilters({ ...filters, [target.value]: target.checked });
+  }
+
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
 
   return (
     <PageList
@@ -24,6 +100,14 @@ export function EventList() {
       newRole="time"
       sorting={sorting}
       setSorting={updateSorting}
+      filters={
+        <EventFilters
+          statuses={filters}
+          onStatusChange={handleStatusChange}
+          search={search}
+          onSearchChange={handleSearchChange}
+        />
+      }
     />
   );
 }
---
+++ front/src/helpers/filtering.js
@@ -0,0 +1,29 @@
+import { useMemo } from "react";
+import { useSearchParams } from "react-router-dom";
+
+export function useFiltering() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const filters = useMemo(() => {
+    if (searchParams && searchParams.getAll("f")) {
+      console.log(searchParams.getAll("f"));
+      return Object.fromEntries(searchParams.getAll("f").map((e) => [e, true]));
+    } else {
+      return null;
+    }
+  }, [searchParams]);
+
+  const setFilters = function (updatedFilters) {
+    let updated = new URLSearchParams(searchParams);
+    updated.delete("f");
+    Object.entries(updatedFilters)
+      .filter((p) => p[1] !== false)
+      .forEach((p) => {
+        updated.append("f", p[0]);
+      });
+
+    setSearchParams(updated);
+  };
+
+  return [filters, setFilters];
+}
