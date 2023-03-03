c8fc5375c13f365fa9c71df8594e5c00c6a838da
superfaz <16510828+superfaz@users.noreply.github.com>
2023-03-03 20:36:02 +0000

    Adapt codespaces configuration

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -15,6 +15,8 @@ echo "REACT_APP_API_ROOT_URL=https://${CODESPACE_NAME}-7268.preview.app.github.d
 # update configuration for API
 dotnet user-secrets set "BaseUrl" "https://${CODESPACE_NAME}-7268.preview.app.github.dev" --project "/workspaces/basic/src/Basic.WebApi"
 dotnet user-secrets set "Cors:Origins" "https://${CODESPACE_NAME}-3000.preview.app.github.dev,https://${CODESPACE_NAME}-7268.preview.app.github.dev" --project "/workspaces/basic/src/Basic.WebApi"
+dotnet user-secrets set "ActiveDirectory:Server" "ldap" --project "/workspaces/basic/src/Basic.WebApi"
+dotnet user-secrets set "EmailService:Server" "smtp" --project "/workspaces/basic/src/Basic.WebApi"
 dotnet user-secrets set "EmailService:FrontBaseUrl" "https://${CODESPACE_NAME}-3000.preview.app.github.dev" --project "/workspaces/basic/src/Basic.WebApi"
 dotnet user-secrets set "DatabaseDriver" "MySql" --project "/workspaces/basic/src/Basic.WebApi"
 dotnet user-secrets set "ConnectionStrings:MySql" "Server=mysql;Database=basic;User=basic;Password=basic;Pooling=False;" --project "/workspaces/basic/src/Basic.WebApi"
