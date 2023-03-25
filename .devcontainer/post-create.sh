56cf42fea61fd8a3b7581fafb74ae8e6ef48343d
superfaz <16510828+superfaz@users.noreply.github.com>
2023-03-25 19:49:12 +0000

    Update post-create with generic configuration

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -21,10 +21,12 @@ fi
 if [ "${CODESPACES}" = "true" ]; then
   dotnet user-secrets set "BaseUrl" "https://${CODESPACE_NAME}-7268.preview.app.github.dev" --project "/workspaces/basic/src/Basic.WebApi"
   dotnet user-secrets set "Cors:Origins" "https://${CODESPACE_NAME}-3000.preview.app.github.dev,https://${CODESPACE_NAME}-7268.preview.app.github.dev" --project "/workspaces/basic/src/Basic.WebApi"
-  dotnet user-secrets set "ActiveDirectory:Server" "ldap" --project "/workspaces/basic/src/Basic.WebApi"
-  dotnet user-secrets set "EmailService:Server" "smtp" --project "/workspaces/basic/src/Basic.WebApi"
   dotnet user-secrets set "EmailService:FrontBaseUrl" "https://${CODESPACE_NAME}-3000.preview.app.github.dev" --project "/workspaces/basic/src/Basic.WebApi"
-  dotnet user-secrets set "DatabaseDriver" "MySql" --project "/workspaces/basic/src/Basic.WebApi"
-  dotnet user-secrets set "ConnectionStrings:MySql" "Server=mysql;Database=basic;User=basic;Password=basic;Pooling=False;" --project "/workspaces/basic/src/Basic.WebApi"
-  dotnet user-secrets set "ConnectionStrings:SqlServer" "Server=mssql;Database=basic;User=basic;Password=basic@Passw0rd;Trusted_Connection=True;MultipleActiveResultSets=true" --project "/workspaces/basic/src/Basic.WebApi"
 fi
+
+# update configuration
+dotnet user-secrets set "ActiveDirectory:Server" "ldap" --project "/workspaces/basic/src/Basic.WebApi"
+dotnet user-secrets set "EmailService:Server" "smtp" --project "/workspaces/basic/src/Basic.WebApi"
+dotnet user-secrets set "DatabaseDriver" "MySql" --project "/workspaces/basic/src/Basic.WebApi"
+dotnet user-secrets set "ConnectionStrings:MySql" "Server=mysql;Database=basic;User=basic;Password=basic;Pooling=False;" --project "/workspaces/basic/src/Basic.WebApi" 1>/dev/null
+dotnet user-secrets set "ConnectionStrings:SqlServer" "Server=mssql;Database=basic;User=basic;Password=basic@Passw0rd;Trusted_Connection=True;MultipleActiveResultSets=true" --project "/workspaces/basic/src/Basic.WebApi" 1>/dev/null
