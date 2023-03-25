1a92d60c9b179d9fd2c370d5b82189e3c465f52e
superfaz <16510828+superfaz@users.noreply.github.com>
2023-03-25 12:37:21 +0000

    Fix post-create for devcontainer

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# add to safe repository
+git config --global --add safe.directory ${PWD}
+
 # load dependencies
 cd front && yarn
 cd -
@@ -10,14 +13,18 @@ chmod +x setup-https.sh
 ./setup-https.sh
 
 # update configuration for front
-echo "REACT_APP_API_ROOT_URL=https://${CODESPACE_NAME}-7268.preview.app.github.dev" >> front/.env.local
+if [ "${CODESPACES}" = "true" ]; then
+  echo "REACT_APP_API_ROOT_URL=https://${CODESPACE_NAME}-7268.preview.app.github.dev" >> front/.env.local
+fi
 
 # update configuration for API
-dotnet user-secrets set "BaseUrl" "https://${CODESPACE_NAME}-7268.preview.app.github.dev" --project "/workspaces/basic/src/Basic.WebApi"
-dotnet user-secrets set "Cors:Origins" "https://${CODESPACE_NAME}-3000.preview.app.github.dev,https://${CODESPACE_NAME}-7268.preview.app.github.dev" --project "/workspaces/basic/src/Basic.WebApi"
-dotnet user-secrets set "ActiveDirectory:Server" "ldap" --project "/workspaces/basic/src/Basic.WebApi"
-dotnet user-secrets set "EmailService:Server" "smtp" --project "/workspaces/basic/src/Basic.WebApi"
-dotnet user-secrets set "EmailService:FrontBaseUrl" "https://${CODESPACE_NAME}-3000.preview.app.github.dev" --project "/workspaces/basic/src/Basic.WebApi"
-dotnet user-secrets set "DatabaseDriver" "MySql" --project "/workspaces/basic/src/Basic.WebApi"
-dotnet user-secrets set "ConnectionStrings:MySql" "Server=mysql;Database=basic;User=basic;Password=basic;Pooling=False;" --project "/workspaces/basic/src/Basic.WebApi"
-dotnet user-secrets set "ConnectionStrings:SqlServer" "Server=mssql;Database=basic;User=basic;Password=basic@Passw0rd;Trusted_Connection=True;MultipleActiveResultSets=true" --project "/workspaces/basic/src/Basic.WebApi"
+if [ "${CODESPACES}" = "true" ]; then
+  dotnet user-secrets set "BaseUrl" "https://${CODESPACE_NAME}-7268.preview.app.github.dev" --project "/workspaces/basic/src/Basic.WebApi"
+  dotnet user-secrets set "Cors:Origins" "https://${CODESPACE_NAME}-3000.preview.app.github.dev,https://${CODESPACE_NAME}-7268.preview.app.github.dev" --project "/workspaces/basic/src/Basic.WebApi"
+  dotnet user-secrets set "ActiveDirectory:Server" "ldap" --project "/workspaces/basic/src/Basic.WebApi"
+  dotnet user-secrets set "EmailService:Server" "smtp" --project "/workspaces/basic/src/Basic.WebApi"
+  dotnet user-secrets set "EmailService:FrontBaseUrl" "https://${CODESPACE_NAME}-3000.preview.app.github.dev" --project "/workspaces/basic/src/Basic.WebApi"
+  dotnet user-secrets set "DatabaseDriver" "MySql" --project "/workspaces/basic/src/Basic.WebApi"
+  dotnet user-secrets set "ConnectionStrings:MySql" "Server=mysql;Database=basic;User=basic;Password=basic;Pooling=False;" --project "/workspaces/basic/src/Basic.WebApi"
+  dotnet user-secrets set "ConnectionStrings:SqlServer" "Server=mssql;Database=basic;User=basic;Password=basic@Passw0rd;Trusted_Connection=True;MultipleActiveResultSets=true" --project "/workspaces/basic/src/Basic.WebApi"
+fi
