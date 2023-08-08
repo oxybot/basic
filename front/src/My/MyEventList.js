7f8553b910368447c3f0e2553411f442bb366fe5
superfaz <16510828+superfaz@users.noreply.github.com>
2023-08-08 12:56:07 +0200

    Enable sorting on my/events

--- front/src/My/MyEventList.js
+++ front/src/My/MyEventList.js
@@ -1,10 +1,12 @@
 import { useLoaderData, useParams } from "react-router-dom";
 import { useDefinition } from "../api";
 import PageList from "../Generic/PageList";
+import { useSorting } from "../helpers/sorting";
 
 export function MyEventList() {
   const { eventId } = useParams();
   const definition = useDefinition("EventForList");
+  const [sorting, updateSorting] = useSorting();
   const elements = useLoaderData();
   if (definition) {
     definition.fields = definition.fields.filter((f) => f.name !== "user");
@@ -22,6 +24,8 @@ export function MyEventList() {
       elements={elements}
       selectedId={eventId}
       texts={texts}
+      sorting={sorting}
+      setSorting={updateSorting}
     />
   );
 }
